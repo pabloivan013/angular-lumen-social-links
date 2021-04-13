b450083fc00f1aa271880f9090e0cb4edc7aba88
pabloivan013 <pabloivan013@hotmail.com>
2021-04-13 01:42:38 -0200

    Adding production script

---
+++ frontend/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp')
+var rimraf = require('rimraf')
+var fs = require('fs')
+var cheerio = require('cheerio')
+
+const PATH = 'frontend'
+
+gulp.task('dist', function(done) {
+   
+    // Remove frontend folder from backend
+    rimraf.sync(`./../backend/public/${PATH}`)
+
+    // Copy dist folder
+    gulp.src([`./dist/${PATH}/**/*`, `!./dist/${PATH}/index.html`]).pipe(
+        gulp.dest(`./../backend/public/${PATH}`)
+    )
+
+    var $ = cheerio.load(fs.readFileSync(`./dist/${PATH}/index.html`, 'utf-8'))
+
+    $('script').each((i,el) => {
+        let oldSrc = $(el).attr('src')
+        $(el).attr('src', `${PATH}/${oldSrc}`)
+    })
+
+    $('link').each((i,el) => {
+        var oldHref = $(el).attr('href')
+        $(el).attr('href',`${PATH}/${oldHref}`)
+    })
+
+    //console.log("$: ", $.html())
+
+    fs.writeFileSync('./../backend/resources/views/index.php', $.html(), 'utf8')
+
+    done()
+})
